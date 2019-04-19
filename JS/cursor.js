eb9c4e0247ec93c2fe3b69720a375650ef47d9d2
evildez <38060451+evildez@users.noreply.github.com>
2019-04-19 16:17:37 -0500

    Fifth Commit
    Audio implementation
    Dropdown Menu on the work

--- JS/cursor.js
+++
@@ -1,6 +0,0 @@
-function displayCursor(event) {
-    var x = event.clientX;
-    var y = event.clientY;
-    document.getElementById('cursor').style.left = (x - 17.5) + "px";
-    document.getElementById('cursor').style.top = (y - 17.5) + "px";
-}
